dfd1beda702eb5a2807ecef4962b74247d6b129a
hanjungv <hanjungv@gmail.com>
2016-08-02 18:47:32 +0900

    add

--- app/models/comment_flea_market.rb
+++ app/models/comment_flea_market.rb
@@ -1,6 +1,6 @@
 # 플리마켓 댓글
 class CommentFleaMarket < ActiveRecord::Base
   belongs_to :user
-  belongs_to :flea_market
+  belongs_to :flea
   has_many :recomment_flea_markets
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,5 +1,5 @@
 # 어떤 셀러가 어떤 플리마켓에 참여했는가
 class Participation < ActiveRecord::Base
-  belongs_to :flea_market
+  belongs_to :flea
   belongs_to :user
 end
