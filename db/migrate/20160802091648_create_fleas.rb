8593f456b03736005fd9d2fd69cf170c8c7d818e
한정 <hanjungv@gmail.com>
2016-08-05 10:37:27 +0900

    db수정, validate 플리마켓 추가

--- db/migrate/20160802091648_create_fleas.rb
+++ db/migrate/20160802091648_create_fleas.rb
@@ -2,14 +2,18 @@ class CreateFleas < ActiveRecord::Migration
   def change
     create_table :fleas do |t|
 
-      # 모집기간
-      t.string :application_period
+      # 모집 시작기간
+      t.datetime :application_start_period
+      # 모집 종료기간
+      t.datetime :application_end_period
       # 모집인원
       t.integer :number_of_recruitment
       # 비고
       t.string :remark
-      # 장소
-      t.string :place
+      # 지역(서울 - 인천 - 경기 - 경북 - 경남 - 충북 - 충남 - 전북 - 전남 - 제주 )
+      t.string :city_place
+      # 상세장소
+      t.string :detail_place
       # 행사날짜
       t.datetime :event_start_date
       # 행사끝날짜
