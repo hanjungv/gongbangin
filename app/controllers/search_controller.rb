adb873ea5b91ac89a4fa0ed1b43c8914635bb721
한정 <hanjungv@gmail.com>
2016-08-12 11:33:56 +0900

    검색기능수정(이메일, 닉네임 동시검색, 결과 출력 스코어 랭킹순으로)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,13 @@
 class SearchController < ApplicationController
   def user
-    @search = User.where('nickname LIKE ?', "%#{params[:search]}%")
+    @search = User.where('nickname LIKE :search OR email LIKE :search', search: "%#{params[:search]}%").order('score desc')
     render "search/user"
   end
 
   def flea
+  end
+
+  def search_main
+    @all_user = User.order('score desc')
   end
 end
