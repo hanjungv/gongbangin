6f217459ca132ff472cb853fb1ad9190e907fe9c
한정 <hanjungv@gmail.com>
2016-08-17 11:50:26 +0900

    공지사항완료

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
   get 'mastermenu/write_notice'
   post 'create_notice' =>"mastermenu#create_notice"
   get 'destroy_notice/:notice_id' =>"mastermenu#destroy_notice"
-  get 'update_item/:notice_id' => "mastermenu#update_notice"
-  post 'real_update_item/:notice_id'=>"mastermenu#real_update_notice"
+  get 'mastermenu/update_notice/:notice_id' => "mastermenu#update_notice"
+  post 'real_update_notice/:notice_id'=>"mastermenu#real_update_notice"
 
   get 'show_notice/:notice_id' => "mastermenu#show_notice"
   #search#
