ef8fefa4ee710f271a237180da626a9444efe145
LEE <kyeongyong1@naver.com>
2016-08-22 17:21:10 +0900

    update join_flea

--- app/controllers/join_flea_controller.rb
+++ app/controllers/join_flea_controller.rb
@@ -1,7 +1,28 @@
 class JoinFleaController < ApplicationController
   def index
+    @fleas = Flea.where('application_end_date > ?',Time.now).order("application_end_date desc")
   end
 
   def join
+
+    @isThere = FleaSeller.where(flea_id:params[:flea_id]).find(current_user.id)
+
+    if @isThere.nil?
+      flash[:notice] = "이미 등록되었습니다"
+      redirect_to "/"
+    end
+    @flea_seller = FleaSeller.new
+
+    @flea_seller.user_id = current_user.id
+    @flea_seller.flea_id = params[:flea_id]
+
+    @flea_seller.save
+
+    @flea = Flea.find(params[:flea_id])
+    @flea.join_member = @flea.join_member + 1
+
+    @flea.save
+
+    redirect_to "/join_flea/index"
   end
 end
