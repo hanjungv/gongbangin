7cef4414f7e8235c17ac283ae4e74b54f3d0a629
ChaeMingyun <coalsrbs7@gmail.com>
2016-08-27 04:57:36 +0900

    오늘의 마켓 검색 코드 수정

--- app/controllers/fleas_controller.rb
+++ app/controllers/fleas_controller.rb
@@ -18,33 +18,28 @@ class FleasController < ApplicationController
           @past_fleas.append(p)
         elsif date <= p.event_start_date
           @future_fleas.append(p)
-          puts "orange"
-          @search_city = params[:city]
-          if @search_city == '전체'
-            @today_flea_a = Flea.all
-          elsif @search_city == nil
-            @today_flea_a = Flea.all
-          else
-            @today_flea_a = Flea.where(city_place: @search_city)
-          end
-          if params[:search] == ""
-            @search_time = Time.now
-          else
-            @search_time = params[:search]
-          end
-          date = @search_time.to_s.to_date
-          @search_flea = Array.new
-          unless @search_time.nil?
-            @today_flea_a.each do |p|
-              if date <= p.event_end_date.to_date
-                @search_flea.append(p)
-              end
-            end
-          end
-          @f = Array.new
-          @f = @now_fleas
-          # todo 정렬을 해야된다면 어떤 기준으로 정렬을 해야될지...
         end
+      end
+    end
+
+    # 지역이랑 날짜로 검색
+    @search_city = params[:city]
+    @search_time = params[:search]
+    if @search_time == ''
+      @search_time = Time.now
+    end
+
+    if @search_city == '전체'
+      @today_flea_a = Flea.all
+    else
+      @today_flea_a = Flea.where(city_place: @search_city)
+    end
+
+    @search_flea = Array.new
+    date = @search_time.to_s.to_date
+    @today_flea_a.each do |p|
+      if p.event_start_date.to_date <= date && date <= p.event_end_date
+        @search_flea.append(p)
       end
     end
   end
@@ -118,4 +113,5 @@ class FleasController < ApplicationController
   def flea_params
     params.require(:flea).permit(:name, :application_start_date, :application_end_date, :number_of_recruitment, :remark, :city_place, :detail_place, :event_start_date, :event_end_date, :entrance_fee, :user_id, :join_type, :item_count, :agree1, :agree2, :agree3, :agree4, :agree5, :poster_url)
   end
+
 end
