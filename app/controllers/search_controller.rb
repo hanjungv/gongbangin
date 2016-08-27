9b7e198d2680a58815f48cba71b3b01bd90aee7d
ChaeMingyun <coalsrbs7@gmail.com>
2016-08-27 15:54:25 +0900

    중복돼서 쓸모없어진 기능 삭제함

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -16,37 +16,4 @@ class SearchController < ApplicationController
     @all_user = User.where(level: 'seller').order('score desc')
   end
 
-  # 플리마켓 정보 출력 및 날짜와 지역으로 검색하는 기믕
-  def flea_city_place
-    @search_flea = Flea.where('city_place LIKE :search', search: "%#{params[:search]}%")
-  end
-
-  def flea_application_date
-    @search = params[:search]
-    date = @search.to_s.to_date
-    @search_flea_a = Flea.all
-    @search_flea = Array.new
-    unless @search.nil?
-      @search_flea_a.each do |p|
-        if p.application_start_date.to_date <= date && date <= p.application_end_date.to_date
-          @search_flea.append(p)
-        end
-      end
-    end
-  end
-
-  def flea_event_date
-    @search = params[:search]
-    date = @search.to_s.to_date
-    @search_flea_a = Flea.all
-    @search_flea = Array.new
-    unless @search.nil?
-      @search_flea_a.each do |p|
-        if p.event_start_date.to_date <= date && date <= p.event_end_date.to_date
-          @search_flea.append(p)
-        end
-      end
-    end
-  end
-
 end
