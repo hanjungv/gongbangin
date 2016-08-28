ba1e56db66768fdc6bb006cbcbf5024d14d36d1b
한정 <hanjungv@gmail.com>
2016-08-28 15:29:16 +0900

    수정중

--- app/helpers/fleas_helper.rb
+++ app/helpers/fleas_helper.rb
@@ -5,17 +5,13 @@ module FleasHelper
         link_to '로그인해 주세요.', new_user_session_path
       end
     elsif hasLike
-      content_tag :h3 do
         link_to like_flea_path(@flea), method: :post do
-          content_tag :i, nil, class: 'btn btn-primary btn-info fa fa-thumbs-o-up', 'aria-hidden' => 'true'
+          content_tag :i, nil, class: 'fa fa-thumbs-o-up', 'aria-hidden' => 'true'
         end
-      end
     else
-      content_tag :h3 do
         link_to dislike_flea_path(@flea), method: :post do
-          content_tag :i, nil, class: 'btn btn-priamy btn-info fa fa-thumbs-o-down', 'aria-hidden' => 'true'
+          content_tag :i, nil, class: 'fa fa-thumbs-o-down', 'aria-hidden' => 'true'
         end
       end
-    end
   end
 end
